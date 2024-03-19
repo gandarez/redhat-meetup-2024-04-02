8a25f3e6b2435e1167b39de0f0baea8bf7901646
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2024-03-18 23:28:24 -0300

    Add instructions for challenge #1

---
+++ cmd/video-game-api/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gandarez/redhat-meetup-2024-04-02/internal/config"
+	"github.com/gandarez/redhat-meetup-2024-04-02/internal/server"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Setup logger
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+	}))
+
+	// Load configuration from env vars.
+	cfg, err := config.Load(ctx, "./.env")
+	if err != nil {
+		logger.Error("failed to load configuration", slog.Any("error", err))
+
+		os.Exit(1)
+	}
+
+	// setup server
+	httpserver := server.New(cfg.Server.Port,
+		server.WithRecover(logger),
+		server.WithDecompress(),
+		server.WithGzip(),
+	)
+
+	// Add default routes for health check
+	httpserver.AddRoute(server.ReadinessRoute())
+	httpserver.AddRoute(server.LivenessRoute())
+
+	// start httpserver
+	go func() {
+		if err := httpserver.Start(); err != http.ErrServerClosed {
+			logger.Error("failed to start server", slog.Any("error", err))
+		}
+	}()
+
+	logger.Info("http server started", slog.Int("port", cfg.Server.Port))
+
+	// wait for interrupt signal to gracefully shutdown the server with a timeout of 10 seconds.
+	// Use a buffered channel to avoid missing signals as recommended for signal.Notify
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	logger.Info("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	// shutdown http server
+	if err := httpserver.Shutdown(ctx); err != nil {
+		logger.Error("failed to shutdown server", slog.Any("error", err))
+	}
+
+	logger.Info("server gracefully stopped")
+}
